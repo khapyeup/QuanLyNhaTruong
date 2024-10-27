63b9c463c3b2a7b4149d6dbf8297896552ec386b
khapyeup <khapyeup@gmail.com>
2024-10-27 22:03:48 +0700

    fix css chat

--- client/src/pages/Parent/ParentMessage.jsx
+++ client/src/pages/Parent/ParentMessage.jsx
@@ -28,7 +28,7 @@ function ParentMessage() {
 
               {userChat.map(chat => (
                 <div className='p-2 rounded-lg hover:bg-gray-200 bg-gray-200' key={chat._id}>
-                  <h2 className='font-bold'>Teacher1</h2>
+                  <h2 className='font-bold'>{chat.members[1].username}</h2>
                   <p>Chào cô giáo</p>
                 </div>
               ))}
@@ -37,8 +37,12 @@ function ParentMessage() {
             {/* BenPhai */}
             <div className='flex-1'>
               <header class="bg-white p-4 border">
-                <h1 class="text-2xl font-semibold">Teacher1</h1>
+                <h1 class="text-2xl font-semibold">teacher</h1>
               </header>
+              <form className='flex items-center mt-96'>
+                <input type='text' className='border w-full border-black text-xl p-2'/>
+                <button className='relative right-10 bg-blue-700 rounded-full p-1'>Gửi</button>
+              </form>
             </div>
           </div>
         ) : <p>Không có đoạn chat nào</p>}
